d8831ea29baa0dc5940b8206f71e062fb924facd
Jan Manuel Murillo <149052738+7ckngmad0@users.noreply.github.com>
2024-08-20 22:51:57 +0800

    Add files via upload

--- QRgenerator.py
+++ QRgenerator.py
@@ -1,4 +1,10 @@
 import qrcode
 from PIL import Image 
 
-qr=qrcode.QRCode 
+qr=qrcode.QRCode(version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_H,
+                box_size=10,border=4,)
+qr.add_data("https://github.com/7ckngmad0/Hackathon/blob/main/QRgenerator.py")
+qr.make(fit=True)
+img=qr.make_image(fill_color="red",back_color="blue")
+img.save("QRiosity.png")
